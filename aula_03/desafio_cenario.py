4e6fc89833da385a50a8d2d88576a0fc497b527c
Adelson-Nobrega <lopes_nobre@yahoo.com.br>
2025-09-04 23:47:48 -0300

    Atualização da aula 04

--- aula_03/desafio_cenario.py
+++ aula_03/desafio_cenario.py
@@ -1,7 +1,8 @@
-# Em um contexto corporativo verificou-se a necessidade de enviar uma mensagem
-# repetida vezes para lembrar os profissionais que eles precisam atualizar as senhas
-# de acesso. Desenvolver código que envie 5 mensagens para lembrar o usuário de atualizar
-# a senha.
+"""
+Em um contexto corporativo verificou-se a necessidade de enviar uma mensagem repetidas
+vezes para lembrar os profissionais que eles precisam atualizar as senhas de acesso.
+Desenvolver código que envie 5 mensagens para lembrar o usuário de atualizar a senha.
+"""
 
 usuario = 'lopes_nobre'
 senha = '123654'
@@ -17,22 +18,26 @@ for acesso in range(1, 7, 1):
             print('\nMENU PRINCIPAL')
             print('1 - Trocar senha')
             print('2 - Enviar mensagem')
-            print('3 - Sair')
-            opcao = int(input('\nDigite a opção do menu: '))
-            while opcao == 1 or opcao == 2 or opcao == 3:
+            print('3 - Sair do Menu (Logout)')
+
+            while True:
+                try:
+                    opcao = int(input('\nDigite a opção do menu: '))
+                except ValueError:
+                    print('Opção inválida! Por favor, digite um número.')
+                    continue  # Volta para o início do loop 'while'
+
                 if opcao == 1:
                     senha = input('Digite sua nova senha: ')
-                    opcao = int(input('\nDigite a opção do menu: '))
+                    print('Senha alterada com sucesso!')
                 elif opcao == 2:
                     mensagem = input('\nEscreva a mensagem a ser enviada: ')
                     print(f'\nMensagem {mensagem.upper()} enviada com sucesso!')
-                    opcao = int(input('\nDigite a opção do menu: '))
                 elif opcao == 3:
-                    print('\nSistema encerrado com sucesso!\n')
-                    opcao = 0
+                    print('\nSaindo do menu principal...\n')
+                    break  # Sai do loop do menu
                 else:
                     print('Opção inválida!')
-                    opcao = int(input('\nDigite a opção do menu: '))
         else:
             print('ATENÇÃO! É OBRIGATÓRIO A TROCA DA SENHA!')
             senha = input('Digite sua nova senha: ')
